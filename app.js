4df7b00c3ebb5b7779509dbf5a0afb4aa60f2d3c
John Baird <john.e.baird5@gmail.com>
2022-06-13 03:53:57 -0600

    Finished

---
+++ app.js
@@ -0,0 +1,14 @@
+
+let a = $('.navItem');
+$(a).css('color','white');
+
+let b = $('#nav');
+$(b).css('background-color', '#145');
+
+let o = $('li:odd');
+$(o).css('background-color', '#ccc');
+
+let e = $('li:even');
+$(e).css('background-color', ' #aaa');
+
+$('input[placeholder="First Name"]').val("John");
